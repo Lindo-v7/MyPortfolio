aebd0dd1e6ffa4368048b601bbf5b5a2d7051268
Lindokuhle Vilakazi <lindokuhle.vilakazi@soliditech.com>
2023-09-02 20:07:25 +0200

    adding favicon and making footer links work

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template
 import os
+import datetime as dt
+
+BIRTH_YEAR = 1998
+BIRTH_MONTH = 1
+BIRTH_DAY = 31
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
@@ -7,7 +12,10 @@ app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    age = dt.datetime.today().year - BIRTH_YEAR - 1
+    if dt.datetime.today().month > BIRTH_MONTH:
+        age = age + 1
+    return render_template('index.html', age=age)
 
 
 if __name__ == "__main__":
